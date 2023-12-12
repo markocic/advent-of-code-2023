dbd874360b31bff5fa02e5c2b112e244844373c0
markocic <markokocic2000@gmail.com>
2023-12-13 00:22:54 +0100

    it's just too late to solve this problem. Will try again tomorrow morning.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -19,40 +19,100 @@ fn parse_input(contents: String) -> Vec<Springs> {
 
     for line in contents.split("\n") {
         let split : Vec<&str> = line.split(" ").collect();
-        result.push(Springs {
-            record: split[0].to_string(),
-            damaged: split[1]
+        let damaged = split[1]
                 .split(",")
                 .filter_map(|i| i.parse().ok())
-                .collect()
+                .collect::<Vec<usize>>();
+
+        let swaps_left = damaged.iter().sum::<usize>() - split[0].chars().filter(|i| *i == '?').count();
+        result.push(Springs {
+            record: split[0].to_string(),
+            damaged,
+            swaps_left
         })
     }
 
     result
 }
 
-#[derive(Debug)]
+fn is_valid(record: String, damaged: Vec<usize>) -> bool {
+    
+    let record_count : Vec<usize> = record.split(".")
+        .filter(|i| !i.is_empty() && !i.contains("?"))
+        .map(|i| i.len())
+        .collect();
+
+    println!("Checking {}, it's {}", record, record_count == damaged);
+    record_count == damaged
+}
+
+#[derive(Debug, Clone)]
 struct Springs {
     record: String,
-    damaged: Vec<usize>
+    damaged: Vec<usize>,
+    swaps_left: usize
 }
 
 impl Springs {
 
     fn number_of_combinations(&self) -> usize {
-        0
+        self.compute(&mut self.record.to_string(), 0, self.swaps_left)
     }
 
-    fn compute(&self, length: usize) -> usize {
+    fn compute(&self, record: &mut String, curr: usize, swaps_left: usize) -> usize {
+        if curr >= record.len() { return 0 }
+        let cc = record.chars().nth(curr).unwrap();
 
-        0
+        if swaps_left == 0 {
+            if is_valid(record.to_owned(), self.damaged.clone()) { return 1; }
+            else { return 0; }
+        }
+
+        let mut res = 0;
+
+        if cc == '.' { self.compute(record, curr + 1, swaps_left); }
+
+        let mut first : Vec<char> = record.chars().collect();
+        first[curr] = '.';
+        let mut first_str: String = first.iter().collect();
+        res += self.compute(&mut first_str, curr + 1, swaps_left);
+
+        let mut second : Vec<char> = record.chars().collect();
+        second[curr] = '#';
+        let mut second_str: String = first.iter().collect();
+
+        res += self.compute(&mut second_str, curr + 1, swaps_left - 1);
+
+        res
+
+
+        // if curr >= record.len() { return 0 }
+        //
+        // let cc = record.chars().nth(curr).unwrap();
+        //
+        // if cc != '?' { return 0 }
+        //
+        //
+        // if question_marks > 0 {
+        //     let record_pound = record.replace_range(curr..curr+1, "#");
+        // } else {
+        //
+        // }
+        //
+        // let record_one = record.replace_range(curr..curr+1, ".");
+        // 
+        // 0
     }
 }
 
+
 #[cfg(test)]
 mod test {
-    use crate::process;
+    use std::iter::zip;
 
+    use crate::{process, is_valid};
+
+    #[ignore]
     #[test]
     fn test() {
         let t = "\
@@ -64,5 +124,37 @@ mod test {
 ?###???????? 3,2,1".to_string();
 
         assert_eq!(process(t), 21);
+    }
+
+    #[test]
+    fn simple_test() {
+        let t = "\
+???.### 1,1,3".to_string();
+        assert_eq!(process(t), 1);
+    }
+
+    #[test]
+    fn valid_test() {
+        let tests : Vec<String> = vec![
+            "#.#.###".to_string(),
+            ".#...#....###.".to_string(),
+            ".#.###.#.######".to_string(),
+            "####.#...#...".to_string(),
+            "#....######..#####.".to_string(),
+            ".###.##....#".to_string()
+        ];
+
+        let damaged : Vec<Vec<usize>> = vec![
+            vec![1, 1, 3],
+            vec![1, 1, 3],
+            vec![1, 3, 1, 6],
+            vec![4, 1, 1],
+            vec![1, 6, 5],
+            vec![3, 2, 1]
+        ];
+
+        for (t, d) in zip(tests, damaged) {
+            assert_eq!(is_valid(t, d), true);
+        }
     }
 }
